f6e7556909bdffb598916ba1267da2713ba350ae
Pirajeen Kandasamy <pkandasamy7@myseneca.ca>
2025-09-15 13:47:02 -0400

    added lab1c: integer object with isaac, sorry for delay

---
+++ lab1c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''This script demonstrates integer objects and converting them to strings.'''
+
+# String object
+name = 'Isaac'
+
+# Integer object
+age = 72
+
+# Combine string + integer using str() to convert the integer
+print(name + ' is ' + str(age) + ' years old!')
