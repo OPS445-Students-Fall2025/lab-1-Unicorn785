492cfc12cd78a4d13b8982fb74c1de90a3a5a5fc
Pirajeen Kandasamy <pkandasamy7@myseneca.ca>
2025-09-15 13:52:23 -0400

    added lab1d:math operations , sorry for delay

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''This script demonstrates math operations and operator precedence.'''
+
+# Integer objects
+x = 10
+y = 2
+z = 5
+
+# result of code
+result = x + y * z
+
+# Print the full expression and result
+print(f"{x} + {y} * {z} = {result}")
